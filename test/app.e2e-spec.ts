ef85b71903859487ffae8c619b947693eeae6ad9
Ozifrankly Silva <ozifrankly.silva@seedz.ag>
2024-03-24 11:44:17 -0300

    session 3

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,18 +7,12 @@ describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    // const moduleFixture: TestingModule = await Test.createTestingModule({
+    //   imports: [AppModule],
+    // }).compile();
+    // app = moduleFixture.createNestApplication();
+    // await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it('/ (GET)', () => {});
 });
